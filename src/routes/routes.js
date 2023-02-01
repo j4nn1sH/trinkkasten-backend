741a0469907d43b13f29d31e1eeb7c36ef9854b0
jhoeferlin <j@hoeferlins.de>
2023-02-01 19:15:35 +0100

    :D

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,13 @@
 import express from 'express'
-import checkAuth from '../middlewares/checkAuth.middleware'
 
 // Import all routes
 import authRoutes from './auth.routes'
-import usersRoutes from './users.routes'
 import eventsRoutes from './events.routes'
 import vocabularyRoutes from './vocabulary.routes'
 
 const router = express.Router()
 
 router.use('/auth', authRoutes)
-router.use('/users', usersRoutes)
 router.use('/events', eventsRoutes)
 router.use('/vocabulary', vocabularyRoutes)
 
